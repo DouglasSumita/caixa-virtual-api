1b5ff8a317805d7cfa202f621815a46ce77c8799
Douglas Sumita <douglas@sgsistemas.com.br>
2020-10-19 15:03:07 -0300

    Adicionado injeçao de dependencia para a funcao connect

--- src/util/bancoDados.js
+++ src/util/bancoDados.js
@@ -1,18 +1,16 @@
 const MongooseService = require('../services/MongooseService')
-const mongooseService = new MongooseService()
 const Logger = require('../util/Logger')
 
-mongooseService.setConfig({
-    local: 'mongodb://caixavirtualadm:caixavirtual123@mongo_caixa-virtual:27017/',
-    dataBaseName: 'caixa-virtual',
-    options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }
-})
-
-async function connect() {
+async function connect(mongooseService = new MongooseService()) {
+    mongooseService.setConfig({
+        local: 'mongodb://caixavirtualadm:caixavirtual123@mongo_caixa-virtual:27017/',
+        dataBaseName: 'caixa-virtual',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        }
+    })
     Logger.log(await mongooseService.connect())
 }
 
